f55e61d7a8de69eecca14f00909e401e3afddce7
furu-hoangnguyen <hoang.nguyen@furucrm.com>
2020-02-07 17:51:19 +0700

    firtCommit

---
+++ script.js
@@ -0,0 +1,47 @@
+function reponsiveMenu() {
+    var x = document.getElementById("myheader_nav");
+    if (x.className === "header_nav") {
+        x.className += " responsive";
+    } else {
+        x.className = "header_nav";
+    }
+}
+
+function showAllImg() {
+    for (var i = 1; i < 7; i++) {
+        document.getElementById("img" + i).style.display = 'block';
+    }
+}
+
+function hideAllImg() {
+    for (var i = 1; i < 7; i++) {
+        document.getElementById("img" + i).style.display = 'none';
+    }
+}
+
+function showImg(name) {
+    hideAllImg();
+    var arr = [];
+    switch (name.toString()) {
+        case "brand":
+            arr = ["img1", "img2", "img3", "img6"];
+            arr.map(elm => document.getElementById(elm).style.display = 'block');
+            break;
+        case "graphic":
+            arr = ["img2", "img5", "img1", "img4"];
+            arr.map(elm => document.getElementById(elm).style.display = 'block');
+            break;
+        case "nature":
+            arr = ["img1", "img4", "img6", "img3"];
+            arr.map(elm => document.getElementById(elm).style.display = 'block');
+            break;
+        case "animation":
+            arr = ["img4", "img3", "img1", "img2"];
+            arr.map(elm => {
+                document.getElementById(elm).style.display = 'block';
+            });
+            break;
+        default:
+            break;
+    }
+}
