f7a9f5f4b6859cea64c424cb65583653afb74b16
furu-hoangnguyen <hoang.nguyen@furucrm.com>
2020-02-12 09:54:25 +0700

    hoang_ui

--- script.js
+++ script.js
@@ -80,27 +80,19 @@ function hidePopup() {
     document.getElementById('popup').style.display = 'none';
 }
 
-function operateActionScroll() {
-    console.log("aaaa")
-    var elem = document.getElementById("pageMain");
-    var y = elem.scrollTop;
-    if (y > 50) {
-        alert("al")
-    }
-
-}
-
 function getScroll() {
     if (window.pageYOffset != undefined) {
-        if (pageYOffset > 100) {
+        if (pageYOffset > 50) {
             document.getElementById("myheader_nav").style.backgroundColor = "rgba(250, 250, 250, 0.98)";
             document.getElementById("myheader_nav").style.color = "black";
-            document.getElementById("icon").style.color = "black";
+            document.getElementById("icon").style.color = "white";
+            document.getElementById("iconCollaps").style.backgroundColor = 'rgb(238, 189, 55)';
 
         } else {
             document.getElementById("myheader_nav").style.background = "none";
             document.getElementById("myheader_nav").style.color = "white";
             document.getElementById("icon").style.color = "white";
+            document.getElementById("iconCollaps").style.background = 'none';
         }
     }
 }
