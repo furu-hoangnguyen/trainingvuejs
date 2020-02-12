d8d9d511d0c4b0a5d9b5d6c18607cd59f7b1919b
furu-hoangnguyen <hoang.nguyen@furucrm.com>
2020-02-12 13:24:15 +0700

    hoang_ui_3

--- script.js
+++ script.js
@@ -8,9 +8,11 @@ function reponsiveMenu() {
 }
 
 function showAllImg() {
+    resetColorbtn();
     for (var i = 1; i < 7; i++) {
         document.getElementById("img" + i).style.display = 'block';
     }
+    document.getElementById('btn1').style.color = 'rgb(238, 189, 55)';
 }
 
 function hideAllImg() {
@@ -19,27 +21,38 @@ function hideAllImg() {
     }
 }
 
+function resetColorbtn() {
+    for (var i = 1; i <= 5; i++) {
+        document.getElementById("btn" + i).style.color = 'white';
+    }
+}
+
 function showImg(name) {
+    resetColorbtn();
     hideAllImg();
     var arr = [];
     switch (name.toString()) {
         case "brand":
             arr = ["img1", "img2", "img3", "img6"];
             arr.map(elm => document.getElementById(elm).style.display = 'block');
+            document.getElementById('btn2').style.color = 'rgb(238, 189, 55)';
             break;
         case "graphic":
             arr = ["img2", "img5", "img1", "img4"];
             arr.map(elm => document.getElementById(elm).style.display = 'block');
+            document.getElementById('btn3').style.color = 'rgb(238, 189, 55)';
             break;
         case "nature":
             arr = ["img1", "img4", "img6", "img3"];
             arr.map(elm => document.getElementById(elm).style.display = 'block');
+            document.getElementById('btn4').style.color = 'rgb(238, 189, 55)';
             break;
         case "animation":
             arr = ["img4", "img3", "img1", "img2"];
             arr.map(elm => {
                 document.getElementById(elm).style.display = 'block';
             });
+            document.getElementById('btn5').style.color = 'rgb(238, 189, 55)';
             break;
         default:
             break;
@@ -52,20 +65,31 @@ function hidAllBlogPost() {
     }
 }
 
+function resetColorBlog() {
+    for (var i = 1; i < 5; i++) {
+        document.getElementById("btn_blogPost" + i).style.color = 'black';
+    }
+}
+
 function showBlogPost(name) {
     hidAllBlogPost();
+    resetColorBlog();
     switch (name) {
         case 'blogPost1':
             document.getElementById('blogPost1').style.display = 'flex';
+            document.getElementById('btn_blogPost1').style.color = 'rgb(238, 189, 55)';
             break;
         case 'blogPost2':
             document.getElementById('blogPost2').style.display = 'flex';
+            document.getElementById('btn_blogPost2').style.color = 'rgb(238, 189, 55)';
             break;
         case 'blogPost3':
             document.getElementById('blogPost3').style.display = 'flex';
+            document.getElementById('btn_blogPost3').style.color = 'rgb(238, 189, 55)';
             break;
         case 'blogPost4':
             document.getElementById('blogPost4').style.display = 'flex';
+            document.getElementById('btn_blogPost4').style.color = 'rgb(238, 189, 55)';
             break;
         default:
             break;
